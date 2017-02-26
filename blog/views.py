1cba8d2fdd9acda6f52e3ce94b50ad85a6be00da
tanya95 <tanya14-1995@mail.ru>
2017-02-26 19:16:31 +0300

    New chanche

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from .models import Post
 from django.contrib.auth.models import User
 
 def post_list(request):
-	me = User.objects.get(username='tanya95')
-	Post.objects.create(author=me, title='Sample title2', text='Test2')
-	Post.objects.create(author=me, title='Sample title3', text='Test3')
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    me = User.objects.get(username='tanya95')
+    Post.objects.create(author=me, title='Sample title2', text='Test2')
+    Post.objects.create(author=me, title='Sample title3', text='Test3')
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
