c2a4079bf57af2296e5dcf538f5a5c7823838d55
tanya95 <tanya14-1995@mail.ru>
2017-02-26 23:58:11 +0300

    ff

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.contrib.auth.models import User
+import requests
+url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+query = '''SELECT DISTINCT ?countryLabel ?headOfStateLabel
+{
+  ?country wdt:P31 wd:Q6256;
+           wdt:P35 ?headOfState .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "ru" }
+}
+ORDER BY ?countryLabel
+LIMIT 100
+'''
+data = requests.get(url, params={'query': query, 'format': 'json'}).json()
+presidents = []
+for item in data['results']['bindings']:
+    presidents.append({
+        'country': item['countryLabel']['value'],
+        'head of state': item['headOfStateLabel']['value']})
 
 def post_list(request):
     me = User.objects.get(username='tanya95')
